97eb3c744147af04533c0a52889a4e829554f753
seankelker <seankelker21>
2016-11-18 20:53:17 -0800

    11/18/16

--- src/OverlapGraphs/OverlapGraphs.java
+++ src/OverlapGraphs/OverlapGraphs.java
@@ -5,87 +5,75 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class OverlapGraphs {
-    public static int[] markers = new int[100];
-    public static String[] startStrand = new String[100];
-    public static String[] endStrand = new String[100];
-    public static String[] markerTags = new String[100];
+    public static int[] markers = new int[1000];
+    public static String[] Prefix = new String[1000];
+    public static String[] Suffix = new String[1000];
+    public static String[] markerTags = new String[1000];
     public static int markerSets = 0;
     public static int sets = 0;
     public static int strandLength = 0;
-    public static int fullSetLength = 0;
+    public static int setLength = 0;
 
     public static void main(String[] args) throws FileNotFoundException {
-	String dataSet = new Scanner(new File("rosalind.txt")).useDelimiter("\\A").next();
+	String rawSet = new Scanner(new File("rosalind.txt")).useDelimiter("\\A").next();
+	String dataSet = rawSet.trim();
+	dataSet = dataSet.replaceAll("\\s", "");
 	String[] fullData = new String[dataSet.length()];
-	fullSetLength = dataSet.length();
+	setLength = dataSet.length();
 	for (int i = 0; i < dataSet.length(); i++) {
 	    fullData[i] = dataSet.substring(i, i + 1);
 	}
-	for (int i = 0; i < dataSet.length(); i++) {
+	markers(fullData, dataSet);
+	extrema(fullData);
+	compare(fullData);
 
-	}
-
-	popMarkers(fullData, dataSet);
-	endStrands(fullData);
-	matches(fullData);
     }
 
-    public static void popMarkers(String[] fullData, String dataSet) {
-	for (int i = 0; i < fullSetLength; i++) {
+    public static void markers(String[] fullData, String dataSet) {
+
+	for (int i = 0; i < setLength; i++) {
 	    if (fullData[i].equals(">")) {
 		markers[sets] = i;
 		sets++;
-	    }
 
-	}
-	int tags = 0;
-	strandLength = markers[2] - markers[1];
-	for (int i = 0; i < fullSetLength; i++) {
-	    if (fullData[i].equals(">")) {
-		
-		markerTags[tags] = dataSet.substring(markers[tags]+1, markers[tags] + 14);
-		tags++;
-		 
 	    }
 	}
-	for (int i = 0; i < sets; i++) {
-	    
-	 
-	}
+	markers[sets] = dataSet.length();
+
     }
 
-    public static void endStrands(String[] fullData) {
-
+    public static void extrema(String[] fullData) {
+	int counter = 0;
+	int suffixCounter = 1;
 	for (int i = 0; i < sets; i++) {
-	    startStrand[i] = fullData[markers[i] + 15] + fullData[markers[i] + 16] 
-		    + fullData[markers[i] + 17];
+	    markerTags[counter] = fullData[markers[counter] + 1] + fullData[markers[counter] + 2]
+		    + fullData[markers[counter] + 3] + fullData[markers[counter] + 4] + fullData[markers[counter] + 5]
+		    + fullData[markers[counter] + 6] + fullData[markers[counter] + 7] + fullData[markers[counter] + 8]
+		    + fullData[markers[counter] + 9] + fullData[markers[counter] + 10] + fullData[markers[counter] + 11]
+		    + fullData[markers[counter] + 12] + fullData[markers[counter] + 13];
+	    Prefix[counter] = fullData[markers[counter] + 14] + fullData[markers[counter] + 15]
+		    + fullData[markers[counter] + 16];
 
-	}
-	for (int i = 0; i < sets; i++) {
+	    Suffix[counter] = fullData[markers[suffixCounter] - 3] + fullData[markers[suffixCounter] - 2]
+		    + fullData[markers[suffixCounter] - 1];
 
-	    endStrand[i] = fullData[markers[i] + (strandLength - 4)] + fullData[markers[i] + (strandLength - 3)]
-		    + fullData[markers[i] + (strandLength - 2)];
-
+	    counter++;
+	    suffixCounter++;
 	}
 
     }
 
-    public static void matches(String[] fullData) {
-	int sol = 0;
-	for (int i = 0; i < sets; i++) {
-	    for (int a = 0; a < sets; a++) {
-		
-		if (a == i && startStrand[a].equals(endStrand[i])) {
-		   
-		} 
-		else if (startStrand[a].equals(endStrand[i])) {
-		   System.out.println(markerTags[i] +" "+ markerTags[a]);
-		   
+    public static void compare(String[] fullData) {
+
+	for (int suffix = 0; suffix < sets; suffix++) {
+	    for (int prefix = 0; prefix < sets; prefix++) {
+
+		if (Suffix[suffix].equals(Prefix[prefix]) && suffix != prefix) {
+		    System.out.println(markerTags[suffix] + " " + markerTags[prefix]);
 		}
 
 	    }
-
 	}
-
     }
+
 }
